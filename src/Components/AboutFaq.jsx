740316fdfd9beb2274232928198a7031e44fe7b7
adamabu578 <abukamsulum01@gmail.com>
2024-05-12 08:59:38 -0700

    faq disply updated

--- src/Components/AboutFaq.jsx
+++ src/Components/AboutFaq.jsx
@@ -5,7 +5,7 @@ const AboutFaq = ({ heading, details }) => {
     <div>
       <div className="h-[334px] bg-gradient text-white flex flex-col items-center justify-center md:px-20 px-5">
         <h1 className="font-bold text-2xl mb-5 text-center">{heading}</h1>
-        <p className="max-w-[800px] text-center">{details}</p>
+        <p className="max-w-[750px] text-center">{details}</p>
       </div>
     </div>
   );
